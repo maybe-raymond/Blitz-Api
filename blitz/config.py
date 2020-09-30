ca03a72d15e6c2ca4f54fa2eb5d9a1b3f924af25
maybe-raymond <mugandiwaraymond@gmail.com>
2020-09-30 23:07:17 +0200

    traversy

--- blitz/config.py
+++ blitz/config.py
@@ -13,3 +13,4 @@ class Development(Config):
 
 class Production(Config):
     TESTING = False
+    SQLALCHEMY_DATABASE_URI ='postgres://jsgtcavdpsaxvp:0a64640ae21b5d75f0ea8ad3ffd694054d818f0b3114b3869386860089c2c48f@ec2-52-23-86-208.compute-1.amazonaws.com:5432/d9a8g1lk1e6mi5'
