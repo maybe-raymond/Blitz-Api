5a6a2bfbb502109bf1dd3898c4ff5c313373c931
maybe-raymond <62111637+maybe-raymond@users.noreply.github.com>
2020-10-03 11:41:31 +0200

    Update config.py

--- blitz/config.py
+++ blitz/config.py
@@ -1,8 +1,10 @@
+import os
+
 class Config(object):
-    SECRET_KEY = '\x00\x8c\xe1^\xa9\xfa\xb4\x80h\xf9o\xa8\xc3\xa5\xa3\x12'
+    SECRET_KEY =  os.environ("very_long_key")
     DEBUG = False
     TESTING = False
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:dogbatman10@localhost/blitzdatabase'
+    SQLALCHEMY_DATABASE_URI = os.environ("postgres_local")
     JSON_SORT_KEYS = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
@@ -13,4 +15,4 @@ class Development(Config):
 
 class Production(Config):
     TESTING = False
-    SQLALCHEMY_DATABASE_URI ='postgres://jsgtcavdpsaxvp:0a64640ae21b5d75f0ea8ad3ffd694054d818f0b3114b3869386860089c2c48f@ec2-52-23-86-208.compute-1.amazonaws.com:5432/d9a8g1lk1e6mi5'
+    SQLALCHEMY_DATABASE_URI = os.environ("postgres_heroku")
